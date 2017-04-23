f8a1498758a5c80e0acfcb6efd09220071a9d51d
justkag8 <justka_g8@wp.pl>
2017-04-23 16:00:16 +0200

    Rejestracja i logowanie

---
+++ www/js/js.js
@@ -0,0 +1,16 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function rejestrowanie() {
+	window.location.assign("formularzrejestracji.html")
+}
+
+function logowanie() {
+	window.location.assign("logowanie.html")
+}
+
